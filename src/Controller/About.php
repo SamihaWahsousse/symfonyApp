23885635e85ce101efc64cc113af89011735eaa6
Samiha Wahsousse <wahsousse.samiha@gmail.com>
2023-02-16 14:07:48 +0100

    first commit "symfony environnement"

---
+++ src/Controller/About.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class About extends AbstractController
+{
+    /**
+     * @Route("about")
+     */
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+        $name   = "samiha";
+
+        return $this->render('about.html.twig', [
+            'number' => $number,
+            'name'   => $name
+        ]);
+    }
+}
